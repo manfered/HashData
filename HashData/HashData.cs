593338bb513aa34ac4e6ad46ea2ded208387b3f6
manfered <35757736+manfered@users.noreply.github.com>
2018-08-04 13:02:39 -0600

    Add project files.

---
+++ HashData/HashData.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashData
+{
+    public class HashData
+    {
+        public static byte[] ComputeHashSha1(byte[] toBeHashed)
+        {
+            using(var sha1 = SHA1.Create())
+            {
+                return sha1.ComputeHash(toBeHashed);
+            }
+        }
+
+        public static byte[] ComputeHashSha256(byte[] toBeHashed)
+        {
+            using(var sha256 = SHA256.Create())
+            {
+                return sha256.ComputeHash(toBeHashed);
+            }
+        }
+
+        public static byte[] ComputeHashSha512(byte[] toBeHashed)
+        {
+            using(var sha512 = SHA512.Create())
+            {
+                return sha512.ComputeHash(toBeHashed);
+            }
+        }
+
+        public static byte[] ComputeHashMd5(byte[] toBeHashed)
+        {
+            using(var Md5 = MD5.Create())
+            {
+                return Md5.ComputeHash(toBeHashed);
+            }
+        }
+    }
+}
---
+++ HashData/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string firstMessage = "First Message To Hash";
+            const string secondMessage = "Second Message To Hash";
+
+            Console.WriteLine("Secure HashData");
+            Console.WriteLine("-------------------------------------------");
+            Console.WriteLine();
+            Console.WriteLine($"First Message : {firstMessage}");
+            Console.WriteLine($"Second Message : {secondMessage}");
+            Console.WriteLine();
+
+            var md5HashedMessage = HashData.ComputeHashMd5(Encoding.UTF8.GetBytes(firstMessage));
+            var md5HashedMessage2 = HashData.ComputeHashMd5(Encoding.UTF8.GetBytes(secondMessage));
+
+            var sha1HashedMessage = HashData.ComputeHashSha1(Encoding.UTF8.GetBytes(firstMessage));
+            var sha1HashedMessage2 = HashData.ComputeHashSha1(Encoding.UTF8.GetBytes(secondMessage));
+
+            var sha256HashedMessage = HashData.ComputeHashSha256(Encoding.UTF8.GetBytes(firstMessage));
+            var sha256HashedMessage2 = HashData.ComputeHashSha256(Encoding.UTF8.GetBytes(secondMessage));
+
+            var sha512HashedMessage = HashData.ComputeHashSha512(Encoding.UTF8.GetBytes(firstMessage));
+            var sha512HashedMessage2 = HashData.ComputeHashSha512(Encoding.UTF8.GetBytes(secondMessage));
+
+            Console.WriteLine();
+            Console.WriteLine("MD5 Hashes");
+            Console.WriteLine();
+            Console.WriteLine($"Message 1 hash = {Convert.ToBase64String(md5HashedMessage)}");
+            Console.WriteLine($"Message 2 hash = {Convert.ToBase64String(md5HashedMessage2)}");
+            Console.WriteLine();
+            Console.WriteLine("Sha1 Hashes");
+            Console.WriteLine($"Message 1 hash = {Convert.ToBase64String(sha1HashedMessage)}");
+            Console.WriteLine($"Message 2 hash = {Convert.ToBase64String(sha1HashedMessage2)}");
+            Console.WriteLine();
+            Console.WriteLine("Sha256 Hashes");
+            Console.WriteLine($"Message 1 hash = {Convert.ToBase64String(sha256HashedMessage)}");
+            Console.WriteLine($"Message 2 hash = {Convert.ToBase64String(sha256HashedMessage2)}");
+            Console.WriteLine();
+            Console.WriteLine("Sha512 Hashes");
+            Console.WriteLine($"Message 1 hash = {Convert.ToBase64String(sha512HashedMessage)}");
+            Console.WriteLine($"Message 2 hash = {Convert.ToBase64String(sha512HashedMessage2)}");
+
+            Console.ReadLine();
+
+        }
+    }
+}
